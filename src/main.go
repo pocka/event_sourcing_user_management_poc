8bbdcbcc16d3394ba2a8cb813161f07eafc25ddc
Shota FUJI <pockawoooh@gmail.com>
2025-03-30 15:49:15 +0900

    Source code formatting setup

---
+++ src/main.go
@@ -0,0 +1,18 @@
+// Copyright 2025 Shota FUJI
+//
+// This source code is licensed under Zero-Clause BSD License.
+// You can find a copy of the Zero-Clause BSD License at LICENSES/0BSD.txt
+// You may also obtain a copy of the Zero-Clause BSD License at
+// <https://opensource.org/license/0bsd>
+//
+// SPDX-License-Identifier: 0BSD
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}

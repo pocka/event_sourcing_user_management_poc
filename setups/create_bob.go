eaaac50e3f5c5877af0419233dd136edb1f7310e
Shota FUJI <pockawoooh@gmail.com>
2025-03-31 22:47:41 +0900

    Create demo viewer user

---
+++ setups/create_bob.go
@@ -0,0 +1,53 @@
+// Copyright 2025 Shota FUJI
+//
+// This source code is licensed under Zero-Clause BSD License.
+// You can find a copy of the Zero-Clause BSD License at LICENSES/0BSD.txt
+// You may also obtain a copy of the Zero-Clause BSD License at
+// <https://opensource.org/license/0bsd>
+//
+// SPDX-License-Identifier: 0BSD
+
+package setups
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/google/uuid"
+	"google.golang.org/protobuf/proto"
+
+	"pocka.jp/x/event_sourcing_user_management_poc/auth"
+	"pocka.jp/x/event_sourcing_user_management_poc/events"
+	"pocka.jp/x/event_sourcing_user_management_poc/gen/event"
+	"pocka.jp/x/event_sourcing_user_management_poc/gen/model"
+)
+
+// CreateBob creates a new viewer user named "Bob" with demo password of
+// "Bob's password".
+// CreateBob returns an ID of the created user on success.
+func CreateBob(db *sql.DB) (string, error) {
+	id := uuid.New().String()
+
+	passwordHash, salt := auth.HashPasswordWithRandomSalt("Bob's password")
+
+	if err := events.Insert(db, []proto.Message{
+		&event.UserCreated{
+			Id:          proto.String(id),
+			DisplayName: proto.String("Bob"),
+			Email:       proto.String("bob@example.com"),
+		},
+		&event.PasswordLoginConfigured{
+			UserId:       proto.String(id),
+			PasswordHash: passwordHash,
+			Salt:         salt,
+		},
+		&event.RoleAssigned{
+			UserId: proto.String(id),
+			Role:   model.Role.Enum(model.Role_ROLE_VIEWER),
+		},
+	}); err != nil {
+		return "", fmt.Errorf("Unable to create Bob: %s", err)
+	}
+
+	return id, nil
+}
